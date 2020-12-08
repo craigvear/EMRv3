d393c648e2e33f3d3d5ce8b55d63ab145ef5f164
craigv <vear2ev2.co.uk>
2020-12-08 18:20:28 +0000

    tidy up - still bugs

--- engine.py
+++ engine.py
@@ -16,7 +16,9 @@ import numpy as np
 
 # --------------------------------------------------
 #
-# instantiate an object for all variables
+#  BORG class following:
+#  https://code.activestate.com/recipes/66531-singleton-we-dont-need-no-stinkin-singleton-the-bo/
+# https://www.oreilly.com/library/view/python-cookbook/0596001673/ch05s23.html
 #
 # --------------------------------------------------
 
@@ -40,26 +42,11 @@ class Borg:
                          'rhythm_rnn': 0,
                          'affect_net': 0}
 
-        # # fill with random values
-        # self.dict_fill()
-        # print(self.datadict)
-
-        # # instantiate nets as objects and make  models
-        # self.move_rnn = MoveRNN()
-        # self.affect_rnn = AffectRNN()
-        # self.move_affect_conv2 = MoveAffectCONV2()
-        # self.affect_move_conv2 = AffectMoveCONV2()
-
         # set out variables
         self.netnames = ['move_rnn',
                          'affect_rnn',
                          'move_affect_conv2',
                          'affect_move_conv2']
-
-        # self.netlist = [self.move_rnn,
-        #                 self.affect_rnn,
-        #                 self.move_affect_conv2,
-        #                 self.affect_move_conv2]
 
         self.rhythm_rate = 0.1
         self.affect_listen = 0
@@ -76,8 +63,18 @@ class MoveRNN(Borg):
         print('MoveRNN initialization')
         self.move_rnn = load_model('training/models/EMR-3_RNN_skeleton_data.nose.x.h5')
 
-    def predict(self, in_val):
-        return self.move_rnn.predict(in_val)
+    def predict(self):
+        # get the current value and reshape ready for input for prediction
+        localval = self.datadict.get('move_rnn')
+        localval = np.reshape(localval, (1, 1, 1))
+
+        # predictions and input with localval
+        self.pred = self.move_rnn.predict(localval)
+        print(f"  'move_rnn' in: {localval} predicted {self.pred}")
+
+        # save to data dict and master move out ONLY 1st data
+        self.datadict['move_rnn'] = self.pred[0][0]
+        self.datadict['master_move_output'] = self.pred
 
 class AffectRNN(Borg):
     def __init__(self):
@@ -85,8 +82,18 @@ class AffectRNN(Borg):
         print('AffectRNN initialization')
         self.affect_rnn = load_model('training/models/EMR-3_RNN_bitalino.h5')
 
-    def predict(self, in_val):
-        return self.affect_rnn.predict(in_val)
+    def predict(self):
+        # get the current value and reshape ready for input for prediction
+        localval = self.datadict.get('affect_rnn')
+        localval = np.reshape(localval, (1, 1, 1))
+
+        # predictions and input with localval
+        self.pred = self.affect_rnn.predict(localval)
+        print(f"  self.affect_rnn in: {localval} predicted {self.pred}")
+
+        # save to data dict and master move out ONLY 1st data
+        self.datadict['affect_rnn'] = self.pred[0][0]
+        self.datadict['master_move_output'] = self.pred
 
 class MoveAffectCONV2(Borg):
     def __init__(self):
@@ -94,8 +101,18 @@ class MoveAffectCONV2(Borg):
         print('MoveAffectCONV2 initialization')
         self.move_affect_conv2 = load_model('training/models/EMR-3_conv2D_move-affect.h5')
 
-    def predict(self, in_val):
-        return self.move_affect_conv2.predict(in_val)
+    def predict(self):
+        # get the current value and reshape ready for input for prediction
+        localval = self.datadict.get('move_affect_conv2')
+        localval = np.reshape(localval, (1, 1, 1))
+
+        # predictions and input with localval
+        self.pred = self.move_affect_conv2.predict(localval)
+        print(f"  'affect_move_conv2' in: {localval} predicted {self.pred}")
+
+        # save to data dict and master move out ONLY 1st data
+        self.datadict['move_affect_conv2'] = self.pred[0][0]
+        self.datadict['master_move_output'] = self.pred
 
 class AffectMoveCONV2(Borg):
     def __init__(self):
@@ -103,14 +120,11 @@ class AffectMoveCONV2(Borg):
         print('AffectMoveCONV2 initialization')
         self.affect_move_conv2 = load_model('training/models/EMR-3_conv2D_affect-move.h5')
 
-    def predicty(self):
-        # self.in_val = in_val
-        # self.input_val_from = str(self.netnames[self.in_val])
-        # print('IN VAL', self.in_val, self.input_val_from)
+    def predict(self):
         # get the current value and reshape ready for input for prediction
         localval = self.datadict.get('affect_rnn')
         localval = np.reshape(localval, (1, 1, 1))
-        print(' predict input to AffectMoveCONV2            ', localval)
+
         # predictions and input with localval
         self.pred = self.affect_move_conv2.predict(localval)
         print(f"  'affect_move_conv2' in: {localval} predicted {self.pred}")
@@ -131,45 +145,15 @@ class AiDataEngine(Borg):
         Borg.__init__(self)
         self.global_speed = speed
 
-        #
-        # self.interrupt_bang = False
-        # self.running = False
-        # self.routing = False
-        #
-        # # make a default dict for the engine
-        # self.datadict = {'move_rnn': 0,
-        #                  'affect_rnn': 0,
-        #                  'move_affect_conv2': 0,
-        #                  'affect_move_conv2': 0,
-        #                  'master_move_output': 0,
-        #                  'user_in': 0,
-        #                  'rnd_poetry': 0,
-        #                  'rhythm_rnn': 0,
-        #                  'affect_net': 0}
-        #
         # fill with random values
         self.dict_fill()
         print(self.datadict)
         #
         # instantiate nets as objects and make  models
         self.move_net = MoveRNN()
-        self.affectnet = AffectRNN()
+        self.affect_net = AffectRNN()
         self.move_affect_net = MoveAffectCONV2()
         self.affect_move_net = AffectMoveCONV2()
-        #
-        # # set out variables
-        # self.netnames = ['move_rnn',
-        #                  'affect_rnn',
-        #                  'move_affect_conv2',
-        #                  'affect_move_conv2']
-        #
-        # self.netlist = [self.move_rnn,
-        #                 self.affect_rnn,
-        #                 self.move_affect_conv2,
-        #                 self.affect_move_conv2]
-        #
-        # # self.rhythm_rate = 0.1
-        # self.affect_listen = 0
 
     # fills the dictionary with rnd values for each key
     def dict_fill(self):
@@ -184,11 +168,24 @@ class AiDataEngine(Borg):
     # --------------------------------------------------
 
     # makes a prediction for a given net and defined input var
+    async def net1(self):
+        while self.interrupt_bang:
+            self.move_net.predict()
+        await trio.sleep(self.rhythm_rate)
+
+    async def net2(self):
+        while self.interrupt_bang:
+            self.affect_net.predict()
+        await trio.sleep(self.rhythm_rate)
+
+    async def net3(self):
+        while self.interrupt_bang:
+            self.move_affect_net.predict()
+        await trio.sleep(self.rhythm_rate)
+
     async def net4(self):
         while self.interrupt_bang:
-            self.affect_move_net.predicty()
-            # todo returning only 1st position. 4 could be used in a randomiser
-            # return pred[0][0]
+            self.affect_move_net.predict()
         await trio.sleep(self.rhythm_rate)
 
     async def random_poetry(self):
@@ -264,14 +261,14 @@ class AiDataEngine(Borg):
             self.interrupt_bang = True
             async with trio.open_nursery() as nursery:
                 # spawning all the nets
-                # print("parent: spawning net1...")
-                # nursery.start_soon(self.nets, 0, 0)
-                #
-                # print("parent: spawning net2...")
-                # nursery.start_soon(self.nets, 1, 1)
-                #
-                # print("parent: spawning net3...")
-                # nursery.start_soon(self.nets, 2, 2)
+                print("parent: spawning net1...")
+                nursery.start_soon(self.net1)
+
+                print("parent: spawning net2...")
+                nursery.start_soon(self.net2)
+
+                print("parent: spawning net3...")
+                nursery.start_soon(self.net3)
 
                 print("parent: spawning net4...")
                 nursery.start_soon(self.net4)
