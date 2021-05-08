d8779e2a88c8bd4b6bd9358c9f9a60b841e0b7a6
craigv <vear2ev2.co.uk>
2021-05-08 11:50:48 +0100

    working on training script to work with JSON's from real dataset

---
+++ training/emr_training_MSTR.py
@@ -0,0 +1,56 @@
+import conv_training as CONV
+import RNN_training as RNN
+from glob import glob
+from pandas import read_json
+
+# consts
+dataset_dir = glob('dataset/*.json')
+
+
+# create array from JSON and pass to
+
+def create_array(location, feature):
+
+    # reset temp array
+    temp_array = []
+
+    # for each JSON set in dataset parse feature and add to temp array
+    for set in dataset_dir:
+        df = read_json(set)
+
+        # parse the values for each line of set
+        for stuff in df.values:
+            temp_array.append(stuff[location][feature])
+
+    return temp_array
+
+
+def main():
+    # generate working arrays
+    bitalino_array = create_array(4, 'bitalino')
+    nose_array = create_array(4, 'skeleton.nose.x')
+
+
+
+    # instantiate training class objects
+    conv = CONV.Training()
+    rnn = RNN.Training()
+
+    # go get 'em cowgirl
+
+    # 1st RNN = affect in(y) - move out(x)
+    x_train = conv.prep_sets(14) # skeleton.nose.x
+    y_train = conv.prep_sets(9) # bitalino
+    conv.train(x_train, y_train, 'affect-move')
+
+    # 2nd RNN = move in(y) - affect out(x)
+    conv.train(y_train, x_train, 'move-affect')
+
+    # 3 skeleton (nose only)
+    rnn.prep_sets('skeleton_data.nose.x', 14)
+
+    # 4 bitalino
+    rnn.prep_sets('bitalino', 9)
+
+if __name__ == "__main__":
+    main()
